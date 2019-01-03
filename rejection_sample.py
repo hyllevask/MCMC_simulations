1dca5f45001df487f9bdd264cca49cf69f307f53
Johan Öhman <johan.ohman@ltu.se>
2019-01-03 09:41:38 +0100

    Added suport for plotting and generated images

--- rejection_sample.py
+++ rejection_sample.py
@@ -9,11 +9,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def NormalDist(RV):
-    
+    #pdf of normal dist
     f = 1/np.sqrt(2*np.pi)*np.exp(-(RV)**2 / (2))
     return f
 
 def Proposal(RV):
+    #Exponential proposol distribution
     g = np.exp(-RV)
     return g
 
@@ -21,7 +22,9 @@ def Rejection(mu,sigma):
     c = np.sqrt(2*np.exp(1)/np.pi)
 
     while True:
+        #sample from exponential
         X = -np.log(np.random.rand())
+        #Sample from uniform
         U = np.random.rand()
         
         proposal = Proposal(X)
@@ -36,15 +39,21 @@ def Rejection(mu,sigma):
             
 if __name__ == "__main__":
     
-    print('Hej!')
     A = Rejection(1,2)
     x_cont = np.linspace(-5,5,10001)   
-    N = 10000;
+    N = 100000;
     test = np.zeros(N)
+    sigma = 3
+    mu = 0
     for i in range(1,N):
-        test[i] = Rejection(0,1)
+        test[i] = Rejection(mu,sigma)
     plt.figure(1)
     plt.clf()
     plt.hist(test,50,density=True)
-    plt.plot(x_cont,NormalDist(x_cont))
+    plt.ylim((0,0.5))
+    plt.xlim((-15,15))
+    #plt.plot(x_cont,NormalDist(x_cont))
+    plt.xlabel("Z")
+    plt.ylabel("Probability density")
+    plt.title("Rejection Sampling, $\mu$ ="+str(mu)+" and $\sigma$ = "+str(sigma))
     #print(test)
